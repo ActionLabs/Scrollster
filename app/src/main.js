b1e35745656034f929bbc6ed1c1c1dba0aa90edf
Bob Owen <bob@bobowen.org>
2014-08-08 11:46:04 -0700

    Added surface to a world scroll view that detects scrolling operations

--- app/src/main.js
+++ app/src/main.js
@@ -3,9 +3,7 @@ define(function(require, exports, module) {
     'use strict';
     // import dependencies
     var Engine = require('famous/core/Engine');
-    var Modifier = require('famous/core/Modifier');
-    var Transform = require('famous/core/Transform');
-    var ImageSurface = require('famous/surfaces/ImageSurface');
+    var WorldScrollView = require('views/WorldScrollView');
 
     // create the main context
     var mainContext = Engine.createContext();
@@ -13,19 +11,7 @@ define(function(require, exports, module) {
     // your app here
     mainContext.setPerspective(1000);
 
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: '/content/images/famous_logo.png',
-        classes: ['backfaceVisibility']
-    });
+    var worldScrollView = new WorldScrollView();
 
-    var initialTime = Date.now();
-    var centerSpinModifier = new Modifier({
-        origin: [0.5, 0.5],
-        transform : function() {
-            return Transform.rotateY(.002 * (Date.now() - initialTime));
-        }
-    });
-
-    mainContext.add(centerSpinModifier).add(logo);
+    mainContext.add(worldScrollView);
 });
---
+++ app/src/views/WorldScrollView.js
@@ -0,0 +1,70 @@
+define(function(require, exports, module) {
+    'use strict';
+    var View          = require('famous/core/View');
+    var Surface       = require('famous/core/Surface');
+
+    var GenericSync   = require('famous/inputs/GenericSync');
+    var MouseSync     = require('famous/inputs/MouseSync');
+    var TouchSync     = require('famous/inputs/TouchSync');
+    var ScrollSync     = require('famous/inputs/ScrollSync');
+
+    GenericSync.register({
+        'mouse': MouseSync,
+        'touch': TouchSync,
+        'scroll': ScrollSync
+    });
+
+    function WorldScrollView() {
+        View.apply(this, arguments);
+
+        _setupScrollRecieverSurface.call(this);
+        _setupScrollInfoSurface.call(this);
+        _handleScroll.call(this);
+    }
+
+    WorldScrollView.DEFAULT_OPTIONS = {
+
+    };
+
+    WorldScrollView.prototype = Object.create(View.prototype);
+    WorldScrollView.prototype.constructor = WorldScrollView;
+
+    function _setupScrollRecieverSurface() {
+        this.scrollRecieverSurface = new Surface({
+            size: [undefined, undefined] // Take up the entire view
+        });
+
+        this.add(this.scrollRecieverSurface);
+    }
+
+    function _setupScrollInfoSurface() {
+        this.scrollInfo = new Surface({
+            size: [200, 200],
+            content: 'Scroll Value: ',
+            properties: {
+                backgroundColor: 'white'
+            }
+        });
+
+        this.add(this.scrollInfo);
+    }
+
+    function _handleScroll() {
+        var sync = new GenericSync(
+            ['touch', 'scroll'],
+            {direction: GenericSync.DIRECTION_Y}
+        );
+
+        this.scrollRecieverSurface.pipe(sync);
+
+        sync.on('update', function(data) {
+            window.console.log('Scroll update recieved');
+        }.bind(this));
+
+        sync.on('end', function(data) {
+            window.console.log('Scroll end recieved');
+        }.bind(this));
+    }
+
+    module.exports = WorldScrollView;
+});
