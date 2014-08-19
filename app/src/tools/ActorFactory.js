ee55198ce2798fa86897c490b83c3930c229188e
Bob Owen <bob@bobowen.org>
2014-08-18 18:45:04 -0700

    Can specify sizes, initial position, and location for moveTo using pecentages instead of pixel values.

--- app/src/tools/ActorFactory.js
+++ app/src/tools/ActorFactory.js
@@ -1,6 +1,7 @@
 define(function(require, exports, module) {
     'use strict';
     var ActorView     = require('views/ActorView');
+    var UnitConverter = require('tools/UnitConverter');
     var Surface       = require('famous/core/Surface');
     var ImageSurface  = require('famous/surfaces/ImageSurface');
 
@@ -12,6 +13,7 @@ define(function(require, exports, module) {
     ActorFactory.prototype.makeActor = function(name, type, content, classes, properties, size, scrollStart, scrollStop) {
         var newSurface;
 
+        // Ensure backface visibility is set for all new surfaces.
         if (!classes) {
             classes = ['backfaceVisibility'];
         } else {
--- app/src/tools/Director.js
+++ app/src/tools/Director.js
@@ -2,6 +2,7 @@ define(function(require, exports, module) {
     'use strict';
     var ActorFactory       = require('tools/ActorFactory');
     var ActionFactory      = require('tools/ActionFactory');
+    var UnitConverter      = require('tools/UnitConverter');
 
     function Director() {
         this.actors = {}; // Collection of actors by name.
@@ -15,6 +16,15 @@ define(function(require, exports, module) {
             if (actorDescriptions[actorName].type) {
                 actorDescriptions[actorName].properties.zIndex = actorDescriptions[actorName].zIndex;
             }
+
+            // Make sure size is in pixels.
+            actorDescriptions[actorName].size = _unitsToPixels(actorDescriptions[actorName].size);
+
+            // Make sure position is in pixels.
+            if (actorDescriptions[actorName].position) {
+                actorDescriptions[actorName].position = _unitsToPixels(actorDescriptions[actorName].position);
+            }
+
             var newActor = actorFactory.makeActor(actorName,
                                                   actorDescriptions[actorName].type,
                                                   actorDescriptions[actorName].content,
@@ -34,6 +44,11 @@ define(function(require, exports, module) {
             var actionDesc = actionDescriptions[i];
             var actor = this.actors[actionDesc.actor];
 
+            // If action takes a location, ensure that it's in pixels
+            if (actionDesc.properties && actionDesc.properties.location) {
+                actionDesc.properties.location = _unitsToPixels(actionDesc.properties.location);
+            }
+
             actionFactory.makeAction(actor,
                                      actionDesc.type,
                                      actionDesc.start,
@@ -47,6 +62,26 @@ define(function(require, exports, module) {
             stage.addActor(currActor);
         }
     };
+
+    function _unitsToPixels(initial) {
+        var result =[];
+        for (var i = 0; i <= 1; i++) {
+            var checkVal = '' + initial[i]; // cast to string
+            // Check if units are percentages and adjust is necessary
+            // otherwise units are assumed to be pixels.
+            if (checkVal.charAt(checkVal.length - 1) === '%') {
+                if (i === 0) {
+                    result[i] = UnitConverter.percentageToPixelsX(parseInt(checkVal.slice(0, checkVal.length - 1)));
+                }
+                if (i === 1) {
+                    result[i] = UnitConverter.percentageToPixelsY(parseInt(checkVal.slice(0, checkVal.length - 1)));
+                }
+            } else {
+                result[i] = initial[i];
+            }
+        }
+        return result;
+    }
 
     module.exports = Director;
 });
